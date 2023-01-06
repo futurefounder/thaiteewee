06306146986ce17ca0c07d6706ee4388db2242a7
futurefounder <jsk@posteo.de>
2023-01-06 22:06:20 +0100

    add more buttons, make switch audio work

--- src/data/videos.js
+++ src/data/videos.js
@@ -1,6 +1,59 @@
-// https://slugify.online/
-
 export const videoURLs = [
+  {
+    id: "",
+    type: "",
+    title: "",
+  },
+  {
+    id: "",
+    type: "",
+    title: "",
+  },
+  {
+    id: "",
+    type: "",
+    title: "",
+  },
+  {
+    id: "",
+    type: "",
+    title: "",
+  },
+  {
+    id: "",
+    type: "",
+    title: "",
+  },
+  {
+    id: "",
+    type: "",
+    title: "",
+  },
+  {
+    id: "",
+    type: "",
+    title: "",
+  },
+  {
+    id: "",
+    type: "",
+    title: "",
+  },
+  {
+    id: "",
+    type: "",
+    title: "",
+  },
+  {
+    id: "",
+    type: "",
+    title: "",
+  },
+  {
+    id: "",
+    type: "",
+    title: "",
+  },
   "vcWPJw8Wsho",
   "RCDrE7XJ40s",
   "AiQZKd29u3M",
@@ -18,3 +71,56 @@ export const videoURLs = [
 ];
 
 export default videoURLs;
+
+// export const videoURLs = [
+//   {
+//     id: "vcWPJw8Wsho",
+//     type: "comedy",
+//     title: "Funny Cats Compilation",
+//   },
+//   {
+//     id: "RCDrE7XJ40s",
+//     type: "drama",
+//     title: "The Shawshank Redemption",
+//   },
+//   {
+//     id: "AiQZKd29u3M",
+//     type: "comedy",
+//     title: "The Hangover",
+//   },
+//   // ...
+// ];
+
+// export default videoURLs;
+
+// import { useState } from 'react';
+
+// function MyComponent() {
+//   const [videoType, setVideoType] = useState('all');
+//   const [videos, setVideos] = useState([]);
+
+//   function handleButtonClick(event) {
+//     const newVideoType = event.target.value;
+//     setVideoType(newVideoType);
+//   }
+
+//   function filterVideos() {
+//     if (videoType === 'all') {
+//       return videos;
+//     }
+//     return videos.filter((video) => video.type === videoType);
+//   }
+
+//   return (
+//     <div>
+//       <button value="comedy" onClick={handleButtonClick}>Comedy</button>
+//       <button value="drama" onClick={handleButtonClick}>Drama</button>
+//       <button value="all" onClick={handleButtonClick}>All</button>
+//       <ul>
+//         {filterVideos().map((video) => (
+//           <li key={video.id}>{video.title}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
